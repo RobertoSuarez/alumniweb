239db47d643d9a784813a32bb04d13ae9f6bc4a8
RobertoSuarez <electrosonix12@gmail.com>
2022-02-28 16:41:47 -0500

    implemetación del footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,40 +1,69 @@
 <template>
-	<v-footer class="footer-contenedor d-flex flex-column" >
+	<v-footer dark class="pa-15">
+		<v-container>
+
+			<v-row>
+				<!-- Logo de la universidad -->
+				<v-col cols="12" md="4" >
+						<v-img src="../assets/uteqlogo.png" width="9em" class="mx-auto mx-md-0"></v-img>
+						<p class="text-body-1 font-weight-light text-center text-md-left mt-3">© 2022 Universidad Técnica Estatal de Quevedo derechos reservados</p>
+				</v-col>
+
+				<v-col cols="12" md="6" offset-md="2" class="white--text">
+					<v-row>
+						<v-col cols="12" md="4">
+							<p class="text-h6">Servicios</p>
+
+							<p v-for="(t, index) in servicios" :key="index" class="text-body-1 font-weight-light ma-0 mt-3">
+								<a>{{t}}</a>
+							</p>
+
+						</v-col>
+
+						<v-col cols="12" md="4">
+							<p class="text-h6">Enlaces de interés</p>
+							<p v-for="(enlace, index) in enlaces" :key="index" class="text-body-1 font-weight-light ma-0 mt-3">
+								<a>{{enlace}}</a>
+							</p>
+						</v-col>
+
+						<v-col cols="12" md="4">
+							<p class="text-h6">Contactos</p>
+							<div class="text-body-1 font-weight-light mt-3">prueba@uteq.edu.ec</div>
+							<div class="text-body-1 font-weight-light mt-3">+590 0123456789</div>
+							<div class="mt-3">
+								<v-btn icon large><v-icon>fab fa-facebook</v-icon></v-btn>
+								<v-btn icon large><v-icon>fab fa-linkedin</v-icon></v-btn>
+							</div>
+						</v-col>
+					</v-row>
+				</v-col>
+			</v-row>
+
+		</v-container>
 
 
-			<div class="d-flex flex-row my-5">
-					<v-btn href="https://www.facebook.com" target="_blank" class="mx-5" color="white" fab small>
-						<v-icon size="25" color="#3b5998">fab fa-facebook-square</v-icon>
-					</v-btn>
 
-					<v-btn href="https://ec.linkedin.com/" target="_blank" class="mx-5" color="white" fab small>
-							<v-icon size="25" color="#0e76a8">fab fa-linkedin</v-icon>
-					</v-btn>
-
-					<v-btn href="https://www.tiktok.com/es/" target="_blank" class="mx-5" color="white" fab small>
-							<v-icon size="25" color="#000">fas fa-globe</v-icon>
-					</v-btn>
-			</div>
-
-			<v-divider dark style="width: 100%"></v-divider>
-
-			<span class="text-center grey--text text--lighten-2 my-5">
-				© 2022 Universidad Técnica Estatal de Quevedo derechos reservados
-			</span>
-
-
-		</v-footer>
+	</v-footer>
 </template>
 
 <script>
 export default {
-	name: 'TheFooter'
+	name: 'TheFooter',
+	data() {
+		return {
+			servicios: ['Trabajos', 'Noticias', 'Eventos', 'Empresas'],
+			enlaces: ['UTEQ', 'Biblioteca UTEQ', 'SGA'],
+		}
+	}
 }
 </script>
 
 <style scoped>
 	.footer-contenedor {
-		padding-top: 5em;
 		background: #1B1B1B !important;
+	}
+	a {
+		color: white !important;
 	}
 </style>
